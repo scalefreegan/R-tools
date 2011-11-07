0b3a5afc4014a56597832737b477c836c26aaa01
Aaron Brooks <abrooks@systemsbiology.org>
2011-11-07 09:37:18 -0800

    add plotCI

---
+++ plotCI.R
@@ -0,0 +1,10 @@
+plotCI <- function(data,upperCI,lowerCI=NULL,col=NULL) {
+  # Function plots data with confidence bounds using polygons
+  # Useful for visualizing trends in groups of data without the
+  # noise of many lines
+  # data is a vector or list of data to be plotted
+  # upperCI is a vector or list of distances to be plotted 
+  # upward from data
+  # same goes for lowerCI, but this is plotted on bottom
+  # lengths of data, upperCI, lowerCI should correspond
+}
