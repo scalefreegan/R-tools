3f416d79c8f8cd16f5284dd2bfe10c46f0e8500d
Aaron Brooks <abrooks@systemsbiology.org>
2011-12-29 20:40:49 -0800

    fix

--- loglog.R
+++ loglog.R
@@ -3,5 +3,4 @@
 require(lattice)
 require(latticeExtra)
 
-xyplot(y~x, as.data.frame(data), scales=list(y=list(log=10),x=list(log=10)),
-        yscale.components=yscale.components.log10ticks,xscale.components=xscale.components.log10ticks)
+xyplot(y~x, as.data.frame(data), scales=list(y=list(log=10),x=list(log=10)), yscale.components=yscale.components.log10ticks,xscale.components=xscale.components.log10ticks)
