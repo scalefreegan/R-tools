9f3ad57fcad14e6c96b93dd71eefa87e243bd733
Aaron Brooks <abrooks@systemsbiology.org>
2014-03-03 19:25:44 -0800

    added bioscreen

---
+++ bioscreen.R
@@ -0,0 +1,50 @@
+# functions for parsing bioscreen data
+loadBioscreen <- function(file,sep=",",time_format = c("min","hours")[1] ){
+	data = read.delim(file,sep,header=T)
+	# parse time to minutes
+	parseTime <- function(time,format="min") {
+		sapply(time,function(x) {
+			y = as.numeric(strsplit(as.character(x),split=":")[[1]])
+			if (format=="min") {
+				return(y[1]*60 + y[2] + y[3]/60)
+			} else if (format=="hours") {
+				return(y[1] + y[2]/60 + y[3]/(60*60))
+				} else {
+					return(x)
+				}
+			})
+	}
+	parseColumnNames <- function(n) {
+		# return name:indexes
+		# find unique names - ignore replicates
+		uniqueN <- lapply(n,function(x) {
+			nsplit = strsplit(x,"\\.")[[1]]
+			if (length(nsplit)>1) {
+				if (is.na(as.numeric(nsplit[length(nsplit)]))==T) {
+					nsplit = paste(nsplit[1:length(nsplit)],collapse=".")
+				} else {
+					nsplit = paste(nsplit[1:length(nsplit)-1],collapse=".")
+				}
+			} 
+			return(strsplit(nsplit,split="_")[[1]])
+			})
+		uniqueN_list <- lapply(unique(unlist(uniqueN)),function(x){
+				grep(x,uniqueN)
+			}); names(uniqueN_list) <- unique(unlist(uniqueN))
+	
+		return(uniqueN_list)
+	}
+	QC <- function(data,nameList) {
+		# quick QC to identify suspicious data
+
+	}
+	time = parseTime(data[,1],time_format)
+	data = data[,-1]
+	nameList = parseColumnNames(colnames(data))
+	to.r <- list()
+	to.r$time = time
+	to.r$data = data
+	to.r$replicate.mapping = nameList
+	return(to.r)
+}
+
