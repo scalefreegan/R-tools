83903be12eb86c699b8126e92859781649639485
Aaron Brooks <aaron.brooks@embl.de>
2015-05-04 12:45:17 +0200

    quantile_normalize fcn

---
+++ quantile_normalize.R
@@ -0,0 +1,13 @@
+quantile_normalize <- function(df){
+  df_rank <- apply(df,2,rank,ties.method="min")
+  df_sorted <- data.frame(apply(df, 2, sort))
+  df_mean <- apply(df_sorted, 1, mean)
+   
+  index_to_mean <- function(my_index, my_mean){
+    return(my_mean[my_index])
+  }
+   
+  df_final <- apply(df_rank, 2, index_to_mean, my_mean=df_mean)
+  rownames(df_final) <- rownames(df)
+  return(df_final)
+}
